2b1747f2e0d974999ecc1b4563b7033f19bd97d6
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-22 15:06:15 +0300

    end app

---
+++ app.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('but');
+let input = document.getElementById('input');
+let imge = document.getElementById('img');
+let imageBox = document.getElementById('image-box');
+let api = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+button.addEventListener('click', () => {
+  if (input.value == '') {
+    alert('Please enter a valid URL');
+    return;
+  }
+    imge.src = api + input.value;
+    imageBox.appendChild(imge);
+});
